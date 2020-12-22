0f693b2082d06c18507dafc8387089b553bebb93
jyc5331 <jcosgrovecoding@gmail.com>
2020-12-22 18:12:20 -0500

    search bar generates a card

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,13 @@
-//elements and global variables
-var citySearchBar = document.getElementById("city-input");
-var cityCard = document.getElementById("city-card");
-var currentCityEl = document.getElementById("current-city");
-var dateZone = document.getElementsByClassName("date-zone");
-var currentCityImage = document.getElementsByClassName("current-city-image");
+//elements and global variables change all of these to querySelector
+var citySearchBar = document.querySelector("#city-input");
+var cityCard = document.querySelector("#city-card");
+var currentCityEl = document.querySelector("#current-city");
+var dateZone = document.querySelector("#date-zone");
+var currentCityImage = document.querySelector("#current-city-image");
+var cityCardList = document.querySelector("#city-card");
+var citySearchInput = document.querySelector("#city-input");
+var citySearchButton = document.querySelector("#search-button");
 var apiKey = "429949f7230d365361b50b640f79e338";
-var lat = 20;
-var lon = 20;
 
 //today's date
 var today = new Date();
@@ -14,8 +15,22 @@ var dd = String(today.getDate()).padStart(2, "0");
 var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
 var yyyy = today.getFullYear();
 today = mm + "/" + dd + "/" + yyyy;
+dateZone.textContent = today;
 
 //functions
+
+//city search card creation
+function citySearch(event) {
+  event.preventDefault();
+  var cityName = citySearchInput.value;
+  console.log(cityName);
+  var cityCardEl = document.createElement("li");
+  cityCardEl.classList.add("list-group-item");
+  cityCardEl.textContent = cityName;
+  cityCardList.appendChild(cityCardEl);
+}
+citySearchButton.addEventListener("click", citySearch);
+
 function logResponseCurrent(currentWeather) {
   console.log(currentWeather);
 }
@@ -25,44 +40,70 @@ function logResponseUv(currentUv) {
 function logResponseFive(fiveDayForecast) {
   console.log(fiveDayForecast);
 }
-//apis
-//current weather
-var requestUrlCurrent =
-  "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" +
-  apiKey;
-fetch(requestUrlCurrent)
-  .then(function (responseCurrent) {
+
+function requestCurrentWeather(cityName) {
+  var requestUrlCurrent =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&units=imperial&appid=" +
+    apiKey;
+  return fetch(requestUrlCurrent).then(function (responseCurrent) {
     return responseCurrent.json();
-  })
-  .then(function (dataCurrent) {
-    logResponseCurrent(dataCurrent);
   });
-//uv index
-var requestUrlUv =
-  "http://api.openweathermap.org/data/2.5/uvi?lat=" +
-  lat +
-  "&lon=" +
-  lon +
-  "&appid=" +
-  apiKey;
-fetch(requestUrlUv)
-  .then(function (responseUv) {
+}
+function requestUV(lat, lon) {
+  //uv index API
+  var requestUrlUv =
+    "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    apiKey;
+  return fetch(requestUrlUv).then(function (responseUv) {
     return responseUv.json();
-  })
-  .then(function (dataUv) {
-    logResponseUv(dataUv);
   });
-//five day forecast
-var requestUrlFive =
-  "http://api.openweathermap.org/data/2.5/forecast?q=London&appid=" + apiKey;
-fetch(requestUrlFive)
-  .then(function (responseFive) {
+}
+function requestFive(cityName) {
+  //five day forecast API
+  var requestUrlFive =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=" +
+    apiKey;
+  return fetch(requestUrlFive).then(function (responseFive) {
     return responseFive.json();
-  })
-  .then(function (dataFive) {
-    logResponseFive(dataFive);
   });
+}
+
+function displayWeather(cityName) {
+  requestCurrentWeather(cityName).then(function (currentWeatherData) {
+    console.log(currentWeatherData);
+    document.querySelector("#currentTemperature").textContent =
+      currentWeatherData.main.temp;
+    document.querySelector("#currentHumidity").textContent =
+      currentWeatherData.main.humidity;
+    document.querySelector("#currentWindSpeed").textContent =
+      currentWeatherData.wind.speed;
+    //lat and lon
+    var lat = currentWeatherData.coord.lat;
+    var lon = currentWeatherData.coord.lon;
+    console.log(lat, lon);
+
+    requestUV(currentWeatherData.coord.lat, currentWeatherData.coord.lon).then(
+      function (uvData) {
+        console.log(uvData);
+        document.querySelector("#currentUv").textContent = uvData.value;
+      }
+    );
+  });
+  requestFive(cityName).then(function (fiveData) {
+    console.log(fiveData);
+  });
+}
 
 //local storage
 
 //event listeners and calls
+var cityNew = "London";
+displayWeather(cityNew);
